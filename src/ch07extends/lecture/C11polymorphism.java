4d94e0fa7f94309f2a17f6e6618eecd6aca0e680
Seunggyo <tmdry99@gmail.com>
2023-08-29 17:59:20 +0900

    다형성

---
+++ src/ch07extends/lecture/C11polymorphism.java
@@ -0,0 +1,18 @@
+package ch07extends.lecture;
+
+public class C11polymorphism {
+
+    public static void main(String[] args) {
+        String s = "java";
+        Object o = s;
+
+        int v1 = s.hashCode();
+        int v2 = o.hashCode();
+
+        System.out.println("v1 = " + v1);
+        System.out.println("v2 = " + v2);
+
+        System.out.println(System.identityHashCode(s));
+        System.out.println(System.identityHashCode(o));
+    }
+}
