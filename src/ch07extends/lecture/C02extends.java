daa88025e632888249303c3fe5358627346016af
Seunggyo <tmdry99@gmail.com>
2023-08-29 12:25:07 +0900

    상속에 대한 여려가지 내용

---
+++ src/ch07extends/lecture/C02extends.java
@@ -0,0 +1,27 @@
+package ch07extends.lecture;
+
+public class C02extends {
+
+    public static void main(String[] args) {
+        MySubClass021 o1 = new MySubClass021();
+        o1.name = "spring";
+        o1.method3();
+    }
+}
+
+//MySubclass021 : 하위클래스, 자식클래스, Sub class
+//위 코드에 문법 오류가 없도록 MyClass02를 작성
+class MySubClass021 extends MyClass02 {
+
+}
+
+//MyClass02 : 상위클래스, 부모클래스, Super class
+
+class MyClass02 {
+
+    String name;
+
+    void method3() {
+        System.out.println("MyClass2.method3");
+    }
+}
