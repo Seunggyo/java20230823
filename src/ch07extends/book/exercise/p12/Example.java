d4207b05087b9c3d8508e6cdc00c41c477cf6e37
Seunggyo <tmdry99@gmail.com>
2023-08-30 12:16:59 +0900

    구버전에서 실행하는방법

--- src/ch07extends/book/exercise/p12/Example.java
+++ src/ch07extends/book/exercise/p12/Example.java
@@ -5,6 +5,10 @@ public class Example {
     public static void action(A a) {
         a.method1();
         if (a instanceof C c) {
+            //만약 구버전 자바일경우에는
+            //if (a instanceof C){
+//            C c = new C();
+            //를 따로 넣어줘야 실행이 가능하다.
             c.method2();
         }
     }
