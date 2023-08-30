28a81bbd767fa33deb85b041133a31932151ad9f
Seunggyo <tmdry99@gmail.com>
2023-08-30 15:31:20 +0900

    325,326쪽 추상클래스

---
+++ src/ch07extends/book/sec10/exam01/Phone.java
@@ -0,0 +1,18 @@
+package ch07extends.book.sec10.exam01;
+
+public abstract class Phone {
+
+    String owner;
+
+    Phone(String owner) {
+        this.owner = owner;
+    }
+
+    void turnOn() {
+        System.out.println("전원을 킵니다.");
+    }
+
+    void turnOff() {
+        System.out.println("전원을 끕니다.");
+    }
+}
