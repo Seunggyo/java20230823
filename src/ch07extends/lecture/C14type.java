a7ae3a371f61b6fdafa11254f5a693e400ad68f8
Seunggyo <tmdry99@gmail.com>
2023-08-30 10:51:04 +0900

    타입

---
+++ src/ch07extends/lecture/C14type.java
@@ -0,0 +1,22 @@
+package ch07extends.lecture;
+
+public class C14type {
+
+    //type 두가지 기본타입, 참조타입
+    //기본타입
+    int a = 3;
+
+    //참조타입
+    String b = "java";
+
+    Object o = "java";
+
+}
+
+class Parent {
+
+}
+
+class Child extends Parent {
+
+}
